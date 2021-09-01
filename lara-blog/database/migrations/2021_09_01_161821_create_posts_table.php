a17965de5e2104eb997faa10b1a07c7548ce7ef3
9831065 <najafizadeh21@gmail.com>
2021-09-01 20:52:59 +0430

    Post model attributes and migration added

--- lara-blog/database/migrations/2021_09_01_161821_create_posts_table.php
+++ lara-blog/database/migrations/2021_09_01_161821_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->foreign('creator')->on('users')->references('id')->cascadeOnDelete();
             $table->timestamps();
         });
     }

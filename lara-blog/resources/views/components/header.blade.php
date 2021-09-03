2cc99b532af79babb05a872d305260ccd5aae06a
9831065 <najafizadeh21@gmail.com>
2021-09-04 00:25:40 +0430

    Header refactor

--- lara-blog/resources/views/components/header.blade.php
+++ lara-blog/resources/views/components/header.blade.php
@@ -1,12 +1,26 @@
-<div class="mb-4">
-    <nav class="navbar navbar-expand-lg navbar-secondary bg-secondary">
-        <a class="navbar-brand {{ !request()->routeIs('welcome') ? 'active' : '' }}" href="{{ route('welcome') }}">
-            Home
-        </a>
-        <div class="navbar-nav">
-            <a class="nav-item {{ !request()->routeIs('login.page') ? 'active' : '' }}" href="{{ route('login.page') }}">
-                Login
+<div class="mb-4 rounded">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand {{ request()->routeIs('welcome') ? 'active' : '' }}" href="{{ route('welcome') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-columns-gap" viewBox="0 0 16 16">
+                    <path d="M6 1v3H1V1h5zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12v3h-5v-3h5zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8v7H1V8h5zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6v7h-5V1h5zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z"/>
+                </svg>
             </a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="navbarsExample07" style="">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ request()->routeIs('login.page') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('login.page') }}">
+                            Login
+                        </a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-md-0">
+                    <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+                </form>
+            </div>
         </div>
     </nav>
 </div>

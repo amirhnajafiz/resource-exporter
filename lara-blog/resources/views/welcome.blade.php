0323331fa6bc810f353c830b17be1039c3ae3a5e
9831065 <najafizadeh21@gmail.com>
2021-09-05 09:35:46 +0430

    Welcome view refactor

--- lara-blog/resources/views/welcome.blade.php
+++ lara-blog/resources/views/welcome.blade.php
@@ -15,13 +15,22 @@
                     Just create an account and start communicate with other peoples.
                 </p>
             </div>
-            <a class="btn btn-lg btn-primary d-block m-auto my-4 w-25" href="{{ route('login.page') }}" role="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
-                    <path d="M7.5 1v7h1V1h-1z"/>
-                    <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
-                </svg>
-                Login now
-            </a>
+            <div class="d-flex justify-content-around">
+                <a class="btn btn-lg btn-primary d-inline-block w-25" href="{{ route('login.page') }}" role="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
+                        <path d="M7.5 1v7h1V1h-1z"/>
+                        <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
+                    </svg>
+                    Login now
+                </a>
+                <a class="btn btn-lg btn-success d-inline-block w-25" href="{{ route('register.page') }}" role="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+                    </svg>
+                    Register now
+                </a>
+            </div>
         </div>
     </main>
 @stop

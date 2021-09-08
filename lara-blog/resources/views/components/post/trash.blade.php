88c6984171fc0f04b65c4e6119332001e8475dd4
9831065 <najafizadeh21@gmail.com>
2021-09-08 13:44:54 +0430

    Front refactor

--- lara-blog/resources/views/components/post/trash.blade.php
+++ lara-blog/resources/views/components/post/trash.blade.php
@@ -12,6 +12,11 @@
         @if($errors->any())
             <x-error-box></x-error-box>
         @endif
+        <div class="text-danger text-center my-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+            </svg>
+        </div>
         <div class="p-5 mt-1">
             @if(count($posts) > 0)
                 @foreach($posts as $post)
--- lara-blog/resources/views/components/user/password_change.blade.php
+++ lara-blog/resources/views/components/user/password_change.blade.php
@@ -11,6 +11,11 @@
         @if($errors->any())
             <x-error-box></x-error-box>
         @endif
+        <div class="text-secondary text-center my-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-lock" viewBox="0 0 16 16">
+                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"/>
+            </svg>
+        </div>
         <div class="p-5 mt-3">
             <form action="{{ route('password.change') }}" method="post">
                 @csrf

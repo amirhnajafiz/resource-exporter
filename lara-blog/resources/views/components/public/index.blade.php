b79dec9ba424eaff70ca64a9548c0f1c63255882
9831065 <najafizadeh21@gmail.com>
2021-09-08 16:21:17 +0430

    Posts refactor in index

--- lara-blog/resources/views/components/public/index.blade.php
+++ lara-blog/resources/views/components/public/index.blade.php
@@ -59,6 +59,13 @@
                         <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v6h-2a.5.5 0 0 0-.354.854l2.5 2.5a.5.5 0 0 0 .708 0l2.5-2.5A.5.5 0 0 0 10.5 7.5h-2v-6z"/>
                     </svg>
                 </button>
+                <button class="btn btn-light text-warning">
+                    <a href="{{ route('view.post', $post->id) }}" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+                            <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+                        </svg>
+                    </a>
+                </button>
             </div>
             <div class="mt-2">
                 <form action="{{ route('comment') }}" method="post">
@@ -80,12 +87,18 @@
                 @if(count($post->comments) > 0)
                     @foreach($post->comments as $comment)
                         <div class="border rounded my-1 p-2">
-                            <div>
-                                {{ $comment->user->name }} | {{ $comment->created_at }}
+                            <div class="h6 d-flex align-center flex-wrap">
+                                <span class="d-inline-block rounded-circle bg-success" style="width: 20px; height: 20px;"></span>
+                                <span class="ml-1">
+                                    {{ $comment->user->name }}
+                                </span>
                             </div>
                             <div>
                                 {{ $comment->content }}
                             </div>
+                            <small>
+                                {{ $comment->created_at }}
+                            </small>
                         </div>
                     @endforeach
                 @else

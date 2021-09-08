5695c2a661278a308a88bff7299979f7a4bcf806
9831065 <najafizadeh21@gmail.com>
2021-09-08 13:22:24 +0430

    Comments model and migration added

---
+++ lara-blog/app/Models/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'content'
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- lara-blog/app/Models/Post.php
+++ lara-blog/app/Models/Post.php
@@ -31,4 +31,9 @@ class Post extends Model
     {
         return $this->hasMany(Love::class);
     }
+
+    public function comments(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }

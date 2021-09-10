3d4c689849c055c0d3cab4e0f87ff14394083342
9831065 <najafizadeh21@gmail.com>
2021-09-10 11:15:52 +0430

    User change password request added

--- lara-blog/app/Http/Controllers/traits/user/ChangePassword.php
+++ lara-blog/app/Http/Controllers/traits/user/ChangePassword.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\traits\user;
 
+use App\Http\Requests\UserChangePasswordRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -9,33 +10,11 @@ use Illuminate\Support\Facades\Hash;
 
 trait ChangePassword
 {
-    public function changePassword(Request $request): \Illuminate\Http\RedirectResponse
+    public function changePassword(UserChangePasswordRequest $request): \Illuminate\Http\RedirectResponse
     {
-        $rules = [
-            'old_password' => 'required|max:20|min:3',
-            'password' => 'required|max:20|min:3'
-        ];
+        $validated = $request->validated();
 
-        $messages = [
-            'required' => 'The :attribute field is required!',
-            'max' => 'The :attribute must be less than :size characters.',
-            'min' => 'The :attribute must be at least :size characters.'
-        ];
-
-        $validator = \Illuminate\Support\Facades\Validator::make($request->all(), $rules, $messages);
-
-        $validator->after(function ($validator) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        });
-
-        $validated = $validator->validate();
-
-        if ($request->input('user') != Auth::id()) {
-            return redirect()
-                ->back()
-                ->withErrors(['message' => 'You can\'t edit other peoples password.'])
-                ->withInput();
-        } else if (Hash::check($validated['old_password'], Auth::user()->getAuthPassword())) {
+        if (Hash::check($validated['old_password'], Auth::user()->getAuthPassword())) {
             User::query()
                 ->where('id', '=', Auth::id())
                 ->update(['password' => Hash::make($validated['password'])]);

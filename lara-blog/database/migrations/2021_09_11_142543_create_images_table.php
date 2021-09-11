453e808631d8912f8abbff8d0b9638a7ff24310a
9831065 <najafizadeh21@gmail.com>
2021-09-11 18:57:31 +0430

    Image model added

---
+++ lara-blog/database/migrations/2021_09_11_142543_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 32);
+            $table->string('alt', 32);
+            $table->string('path', 1024);
+            $table->timestamps();
+            $table->softDeletes()
+                ->after('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}

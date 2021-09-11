843ce1c6e4c2168b2f43dac3577ab5bd3df4aefb
9831065 <najafizadeh21@gmail.com>
2021-09-11 18:24:25 +0430

    Added the new migrations for relational tables

---
+++ lara-blog/database/migrations/2021_09_11_135308_create_category_post_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Category::class);
+            $table->foreignIdFor(\App\Models\Post::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_post');
+    }
+}

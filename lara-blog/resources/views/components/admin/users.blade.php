cf33514e385344b40ad2551a01cccfbad6055dc3
9831065 <najafizadeh21@gmail.com>
2021-09-12 11:58:42 +0430

    Refactor in users view

--- lara-blog/resources/views/components/admin/users.blade.php
+++ lara-blog/resources/views/components/admin/users.blade.php
@@ -32,6 +32,7 @@
                     {{ $users[$i]->phone }}
                 </td>
                 <td class="d-flex justify-content-start">
+                    @if($users[$i]->is_admin == 0)
                     <form action="{{ route('delete.user', $users[$i]->id) }}" method="post">
                         @csrf
                         @method('delete')
@@ -41,6 +42,13 @@
                             </svg>
                         </button>
                     </form>
+                    @else
+                        <button class="btn btn-light text-danger" disabled>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
+                            </svg>
+                        </button>
+                    @endif
                 </td>
             </tr>
         @endfor

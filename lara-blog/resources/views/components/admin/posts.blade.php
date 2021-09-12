2fa742c75abe999be3106682eb8e5a1002c77e74
9831065 <najafizadeh21@gmail.com>
2021-09-12 11:45:17 +0430

    Posts and users view by admin set

--- lara-blog/resources/views/components/admin/posts.blade.php
+++ lara-blog/resources/views/components/admin/posts.blade.php
@@ -1,1 +1,65 @@
-<?php
+@extends('components.admin.main')
+
+@section('context')
+    <div class="mb-2">
+        <div class="h4">
+            Posts
+        </div>
+    </div>
+    <table class="table table-hover">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">User</th>
+            <th scope="col">Tags</th>
+            <th scope="col">Categories</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Options</th>
+        </tr>
+        </thead>
+        <tbody>
+        @for($i = 0; $i < count($posts); $i++)
+            <tr>
+                <th scope="row">
+                    {{ $i + 1 }}
+                </th>
+                <td>
+                    {{ $posts[$i]->title }}
+                </td>
+                <td>
+                    {{ $posts[$i]->user->email }}
+                </td>
+                <td>
+                    {{ $posts[$i]->tags->implode('title', ', ') }}
+                </td>
+                <td>
+                    {{ $posts[$i]->categories->implode('title', ', ') }}
+                </td>
+                <td>
+                    {{ $posts[$i]->created_at }}
+                </td>
+                <td class="d-flex justify-content-start">
+                    <form action="{{ route('tags.destroy', $posts[$i]->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-light text-danger">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
+                            </svg>
+                        </button>
+                    </form>
+                    <a href="{{ route('tags.edit', $posts[$i]->id) }}" class="btn btn-light text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                        </svg>
+                    </a>
+                </td>
+            </tr>
+        @endfor
+        </tbody>
+    </table>
+@stop
+
+
--- lara-blog/resources/views/components/admin/users.blade.php
+++ lara-blog/resources/views/components/admin/users.blade.php
@@ -1,1 +1,50 @@
-<?php
+@extends('components.admin.main')
+
+@section('context')
+    <div class="mb-2">
+        <div class="h4">
+            Users
+        </div>
+    </div>
+    <table class="table table-hover">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Options</th>
+        </tr>
+        </thead>
+        <tbody>
+        @for($i = 0; $i < count($users); $i++)
+            <tr>
+                <th scope="row">
+                    {{ $i + 1 }}
+                </th>
+                <td>
+                    {{ $users[$i]->name }}
+                </td>
+                <td>
+                    {{ $users[$i]->email }}
+                </td>
+                <td>
+                    {{ $users[$i]->phone }}
+                </td>
+                <td class="d-flex justify-content-start">
+                    <form action="{{ route('tags.destroy', $users[$i]->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-light text-danger">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
+                            </svg>
+                        </button>
+                    </form>
+                </td>
+            </tr>
+        @endfor
+        </tbody>
+    </table>
+@stop
+

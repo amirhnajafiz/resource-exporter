b877dc767c84fa2e0cd070ccbf7ed1560e843068
9831065 <najafizadeh21@gmail.com>
2021-09-13 14:40:56 +0430

    Main page refactor

--- lara-blog/resources/views/welcome.blade.php
+++ lara-blog/resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-    <main role="main" class="container text-dark w-75" style="margin: 202px auto !important;">
-        <div class="jumbotron">
-            <div class="row">
+    <div class="position-relative overflow-hidden p-0 bg-light text-dark"
+         style="height: 600px; background-image: url('{{ asset('images/main.svg') }}'); background-repeat: no-repeat; background-size: cover; background-attachment: fixed"
+    >
+        <div class="w-50 text-left mx-auto my-5 pt-5">
+            <div class="row pt-4">
                 <h1 class="mb-3 d-inline-block col-lg-2 col-md-12 col-sm-12">
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-emoji-laughing-fill" viewBox="0 0 16 16">
                         <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5c0 .501-.164.396-.415.235C6.42 6.629 6.218 6.5 6 6.5c-.218 0-.42.13-.585.235C5.164 6.896 5 7 5 6.5 5 5.672 5.448 5 6 5s1 .672 1 1.5zm5.331 3a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5zm-1.746-2.765C10.42 6.629 10.218 6.5 10 6.5c-.218 0-.42.13-.585.235C9.164 6.896 9 7 9 6.5c0-.828.448-1.5 1-1.5s1 .672 1 1.5c0 .501-.164.396-.415.235z"/>
@@ -32,5 +34,7 @@
                 </a>
             </div>
         </div>
-    </main>
+        <div class="product-device box-shadow d-none d-md-block"></div>
+        <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
+    </div>
 @stop

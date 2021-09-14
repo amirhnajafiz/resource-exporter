2fbbe66cd48c4d182349b12c9654e7baa3bc8120
9831065 <najafizadeh21@gmail.com>
2021-09-14 10:12:55 +0430

    Check user administrator middleware added

--- lara-blog/app/Http/Middleware/CheckUserAdministrator.php
+++ lara-blog/app/Http/Middleware/CheckUserAdministrator.php
@@ -4,18 +4,26 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class CheckUserAdministrator
+ * @package App\Http\Middleware
+ */
 class CheckUserAdministrator
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Auth::user()->is_admin == 1)
+            return $next($request);
+        else
+            return back(304);
     }
 }

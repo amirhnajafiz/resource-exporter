843e96ce99a87451f53ed9ab18ac66090fef2280
amirhnajafiz <najafizadeh21@gmail.com>
2023-07-29 20:17:58 +0330

    remove: all php files

--- lara-blog/resources/views/components/post/body/content-box.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="h6 border rounded p-4 mb-2">
-    {!! $post->content !!}
-</div>
--- lara-blog/resources/views/components/post/body/image-box.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="my-2 text-center mb-4">
-    <img
-        src="{{ str_starts_with($post->image->path, 'http') ? $post->image->path : asset('storage/' . $post->image->path) }}"
-        alt="{{ $post->image->alt }}"
-        class="rounded d-block m-auto mb-1 w-50" /> <br />
-    <a href="{{ route('download', $post->id) }}" type="button" class="badge badge-primary">
-        Download
-    </a>
-</div>

d208cdff6da4fb66f6f6de0c0181db452a50de96
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-26 11:33:13 +0330

    init: agent

--- internal/mail/agent.go
+++ internal/mail/agent.go
@@ -1,5 +1,21 @@
 package mail
 
+import (
+	"github.com/mailgun/mailgun-go/v4"
+)
+
 type Agent interface {
 	Send(name, namespace string, rules []string, cpuLimit, ramLimit, cpu, ram float64) error
+}
+
+type agent struct {
+	connection *mailgun.MailgunImpl
+}
+
+func New(cfg Config) {
+	conn := mailgun.NewMailgun(cfg.Domain, cfg.Key)
+}
+
+func (a agent) Send(name, namespace string, rules []string, cpuLimit, ramLimit, cpu, ram float64) error {
+	return nil
 }

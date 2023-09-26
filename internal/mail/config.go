7a0d8741147d7768bb2f4096863c38191722b4ca
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-26 11:35:24 +0330

    update: agent configs

--- internal/mail/config.go
+++ internal/mail/config.go
@@ -1,6 +1,9 @@
 package mail
 
 type Config struct {
-	Domain string `koanf:"domain"`
-	Key    string `koanf:"key"`
+	Sender   string `koanf:"sender"`
+	Receiver string `koanf:"receiver"`
+	Subject  string `koanf:"subject"`
+	Domain   string `koanf:"domain"`
+	Key      string `koanf:"key"`
 }
